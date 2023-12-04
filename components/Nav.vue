6a15db75eb911902553916748e7e2855e017018a
deepanshu188 <deepanshubisht188@gmail.com>
2023-12-04 20:09:47 +0530

    made the nav items dynamic

--- components/Nav.vue
+++ components/Nav.vue
@@ -4,9 +4,9 @@
       <a class="btn btn-ghost text-xl">Aizen</a>
     </div>
     <div class="flex-none gap-2">
-      <div class="dropdown dropdown-end" v-if="userInfo">
+      <div class="dropdown dropdown-end">
         <div tabindex="0" role="button" class="btn btn-ghost btn-circle avatar">
-          <div class="w-10 rounded-full">
+          <div class="w-10 rounded-full" v-if="jwt">
             <img v-if='userInfo.avatar' :src="userInfo.avatar" alt='avatar' />
             <div class="avatar placeholder" v-else>
               <div class="bg-neutral text-neutral-content rounded-full w-8">
@@ -14,30 +14,21 @@
               </div>
             </div>
           </div>
+          <button class="btn btn-ghost btn-circle" v-else>
+            <IMenu />
+          </button>
         </div>
         <ul class="mt-3 z-[1] p-2 shadow menu menu-sm dropdown-content bg-base-100 rounded-box w-52">
-          <li>
-            <NuxtLink to="/" class="p-2">
-              <IHome />
-              Home
+          <li v-for="item in privateItems" v-if="jwt" @click="logout(item.name)">
+            <NuxtLink :to="item.link" class="p-2">
+              <component :is='item.icon'></component>
+              {{ item.name }}
             </NuxtLink>
           </li>
-          <li>
-            <NuxtLink to="/profile" class="p-2">
-              <IProfile />
-              Profile
-            </NuxtLink>
-          </li>
-          <li>
-            <NuxtLink to="/communities" class="p-2">
-              <ICommunities />
-              Communities
-            </NuxtLink>
-          </li>
-          <li @click="logout">
-            <NuxtLink to="/login" class="p-2">
-              <ILogout />
-              Logout
+          <li v-for="item in navItems" v-else>
+            <NuxtLink :to="item.link" class="p-2">
+              <component :is='item.icon'></component>
+              {{ item.name }}
             </NuxtLink>
           </li>
         </ul>
@@ -49,6 +40,59 @@
 <script setup lang="ts">
 import { userDetails, getPersonInfo } from '../services/user';
 import { useUserStore } from '@/stores/user';
+import IHome from './icons/IHome.vue'
+import IProfile from './icons/IProfile.vue'
+import ICommunities from './icons/ICommunities.vue'
+import ILogout from './icons/ILogout.vue'
+import IExplore from './icons/IExplore.vue'
+import IUser from './icons/IUser.vue'
+import ISettings from './icons/ISettings.vue'
+
+const commonItems = [
+  {
+    name: 'Home',
+    icon: IHome,
+    link: '/'
+  },
+  {
+    name: 'Explore',
+    icon: IExplore,
+    link: ''
+  },
+  {
+    name: 'Settings',
+    icon: ISettings,
+    link: ''
+  }
+]
+
+const navItems = [
+  ...commonItems,
+  {
+    name: 'Login',
+    icon: IUser,
+    link: '/login'
+  }
+]
+
+const privateItems = [
+  ...commonItems,
+  {
+    name: 'Profile',
+    icon: IProfile,
+    link: '/profile'
+  },
+  {
+    name: 'Communities',
+    icon: ICommunities,
+    link: '/communities'
+  },
+  {
+    name: 'Logout',
+    icon: ILogout,
+    link: '/',
+  }
+]
 
 const userData = useCookie('userData')
 const jwt = userData.value?.jwt
@@ -63,7 +107,10 @@ if (jwt) {
 }
 const userInfo = computed(() => user.data?.person_view?.person)
 
-const logout = () => {
-  userData.value = ''
+const logout = (name: string) => {
+  if (name === 'Logout') {
+    userData.value = ''
+    window.location.reload()
+  }
 }
 </script>
