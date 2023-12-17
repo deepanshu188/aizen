ceb32854225ee9e283d1928a0fdba8affcce0fc6
deepanshu188 <deepanshubisht188@gmail.com>
2023-12-17 18:27:38 +0530

    config - pwa manifest

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,13 +2,22 @@ export default defineNuxtConfig({
   devtools: { enabled: true },
   ssr: false,
   nitro: {
-    ignore: ['/*']
+    ignore: ['/*'],
   },
-  modules: [
-    '@nuxt/image',
-    '@pinia/nuxt',
-    '@vite-pwa/nuxt'
-  ],
+  modules: ['@nuxt/image', '@pinia/nuxt', '@vite-pwa/nuxt'],
+  pwa: {
+    manifest: {
+      name: 'Aizen for Lemmy',
+      short_name: 'Aizen',
+      theme_color: '#ffffff',
+    },
+    client: {
+      installPrompt: true,
+    },
+    devOptions: {
+      enabled: true,
+    },
+  },
   image: {
     quality: 100,
   },
@@ -29,4 +38,4 @@ export default defineNuxtConfig({
       autoprefixer: {},
     },
   },
-})
+});
