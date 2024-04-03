f95f9a5e66098f04ff65f60297865a680065a5d3
deepanshu188 <deepanshubisht188@gmail.com>
2024-04-03 23:56:08 +0530

    fix: updated the prop name in communities component

--- components/community/Communities.vue
+++ components/community/Communities.vue
@@ -1,26 +1,19 @@
 <script setup lang="ts">
-  const router = useRouter();
+const router = useRouter();
 
-  import { useUserStore } from '@/stores/user';
-  const data = useUserStore();
-  const subscriptions = computed(() => data?.user?.my_user?.follows);
+import { useUserStore } from '@/stores/user';
+const data = useUserStore();
+const subscriptions = computed(() => data?.user?.my_user?.follows);
 </script>
 
 <template>
   <section>
     <ul v-if="subscriptions">
-      <li
-        v-for="c in subscriptions"
-        :key="c.id"
-        class="card bg-base-100 shadow-md m-2 p-2 h-12"
-      >
-        <div
-          class="flex gap-2 items-center cursor-pointer"
-          @click="
-            router.push(`c/${c.community.name}?id=${c.community.id}`)
-          "
-        >
-          <Avatar :name="c.community.title" :url="c.community.icon" />
+      <li v-for="c in subscriptions" :key="c.id" class="card bg-base-100 shadow-md m-2 p-2 h-12">
+        <div class="flex gap-2 items-center cursor-pointer" @click="
+          router.push(`c/${c.community.name}?id=${c.community.id}`)
+          ">
+          <Avatar :name="c.community.title" :image="c.community.icon" />
           <p>{{ c.community.title }}</p>
         </div>
       </li>
