0124d6454c5982eeb0e4181729562c0d21925ff0
deepanshu188 <deepanshubisht188@gmail.com>
2024-04-11 14:30:05 +0530

    added: interactions to the user comments

--- components/comments/UserComments.vue
+++ components/comments/UserComments.vue
@@ -1,15 +1,16 @@
 <script setup lang="ts">
   import { useUserStore } from '@/stores/user';
+  import type { iUser } from './comment';
   const props = defineProps(['comments']);
-  import { iUser } from './comment';
 
   const user: iUser = useUserStore();
   const comments = props.comments ?? user?.data?.comments;
+  console.log(comments, 'cmts');
 </script>
 
 <template>
   <ul>
-    <li v-for="{ comment, creator } in comments">
+    <li v-for="{ comment, creator, counts, saved } in comments">
       <div class="card shadow-xl">
         <div class="card-body">
           <div>
@@ -20,6 +21,7 @@
             />
           </div>
           <p>{{ comment.content }}</p>
+          <Interactions :counts="counts" :saved="saved" />
         </div>
       </div>
     </li>
