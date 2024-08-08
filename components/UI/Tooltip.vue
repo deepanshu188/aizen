96d2bf2dff6d20c7600debb19466d3ea9b29f1b9
deepanshu188 <deepanshubisht188@gmail.com>
2024-08-08 22:28:57 +0530

    updated: fixed the post image width on mobile

--- components/UI/Tooltip.vue
+++ components/UI/Tooltip.vue
@@ -1,25 +1,26 @@
 <script setup lang="ts">
-
 type Props = {
-  text: string;
-  position?: 'left' | 'right' | 'top' | 'bottom'
-}
+    text: string;
+    position?: "left" | "right" | "top" | "bottom";
+};
 
 const props = withDefaults(defineProps<Props>(), {
-  position: 'top'
-})
+    position: "top",
+});
 
-const position = computed(() => ({
-  'left': 'tooltip-left',
-  'right': 'tooltip-right',
-  'top': 'tooltip-top',
-  'bottom': 'tooltip-bottom'
-}[props.position]))
-
+const position = computed(
+    () =>
+        ({
+            left: "tooltip-left",
+            right: "tooltip-right",
+            top: "tooltip-top",
+            bottom: "tooltip-bottom",
+        })[props.position],
+);
 </script>
 
 <template>
-  <div :class="[{ tooltip: text }, position]" :data-tip="text">
-    <slot />
-  </div>
+    <div :class="[{ tooltip: text }, position, 'p-0']" :data-tip="text">
+        <slot />
+    </div>
 </template>
