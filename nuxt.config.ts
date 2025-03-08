ccad8f14ef026bf1c08d133c2a170c9c19df15d6
Deepanshu Bisht <deepanshubisht188@gmail.com>
2025-03-08 12:11:47 +0530

    replaced: nuxt font package

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -17,19 +17,13 @@ export default defineNuxtConfig({
     "@pinia/nuxt",
     "@nuxt/test-utils/module",
     "@nuxt/icon",
-    "@nuxtjs/google-fonts",
+    '@nuxt/fonts',
   ],
 
   vite: {
     plugins: [
       tailwindcss(),
     ],
-  },
-
-  googleFonts: {
-    families: {
-      Roboto: true,
-    },
   },
 
   image: {
