d607580cd47f9d9d10150b3a57d0b3a9e7124c37
deepanshu188 <deepanshubisht188@gmail.com>
2025-05-07 08:09:33 +0530

    updated: pwa config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -22,56 +22,21 @@ export default defineNuxtConfig({
   ],
 
   pwa: {
+    devOptions: {
+      enabled: true,
+    },
     manifest: {
       name: "Aizen",
       short_name: "Aizen",
       description: "A web based lemmy client",
-      theme_color: "#000000",
-      icons: [
-        {
-          src: "pwa-192x192.png",
-          sizes: "192x192",
-          type: "image/png",
-        },
-        {
-          src: "pwa-512x512.png",
-          sizes: "512x512",
-          type: "image/png",
-        },
-      ],
+      theme_color: "#ffffff",
       start_url: "/",
+      display: "standalone",
+      background_color: "#000000",
     },
+    registerType: "autoUpdate",
     workbox: {
-      runtimeCaching: [
-        {
-          urlPattern: '/layouts/default.vue',
-          handler: 'CacheFirst',
-          options: {
-            cacheName: 'default-layout-cache',
-            expiration: {
-              maxEntries: 10,
-              maxAgeSeconds: 60 * 60 * 24 * 7, // Cache for 7 days
-            },
-            cacheableResponse: {
-              statuses: [0, 200],
-            },
-          },
-        },
-        {
-          urlPattern: '/layouts/settings.vue',
-          handler: 'CacheFirst',
-          options: {
-            cacheName: 'settings-layout-cache',
-            expiration: {
-              maxEntries: 10,
-              maxAgeSeconds: 60 * 60 * 24 * 7, // Cache for 7 days
-            },
-            cacheableResponse: {
-              statuses: [0, 200],
-            },
-          },
-        },
-      ],
+      globPatterns: ["**/*.{js,css,html,ico,png,svg,txt,xml,webmanifest}"],
     },
   },
 
