aa0cf3712cf45a42feba160ed36db66441be59f8
deepanshu188 <deepanshubisht188@gmail.com>
2025-05-07 08:54:02 +0530

    added: app icons

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -33,6 +33,18 @@ export default defineNuxtConfig({
       start_url: "/",
       display: "standalone",
       background_color: "#000000",
+      "icons": [
+        {
+          "src": "/nuxt-icon-sm.webp",
+          "sizes": "192x192",
+          "type": "image/png"
+        },
+        {
+          "src": "/nuxt-icon-md.webp",
+          "sizes": "512x512",
+          "type": "image/png"
+        }
+      ]
     },
     registerType: "autoUpdate",
     workbox: {
