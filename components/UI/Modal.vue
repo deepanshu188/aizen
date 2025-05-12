3954690949e0ac557790a09da4d9f5104edab052
deepanshu188 <deepanshubisht188@gmail.com>
2025-05-12 20:36:31 +0530

    updated: logout modal

--- components/UI/Modal.vue
+++ components/UI/Modal.vue
@@ -2,19 +2,21 @@
 interface Props {
   title?: string;
 }
-const { title = 'About' } = defineProps<Props>();
+const { title = "About" } = defineProps<Props>();
 </script>
 
 <template>
-  <dialog id="popup" class="modal">
+  <dialog id="popup" class="modal backdrop-blur-[5px]">
     <div class="modal-box">
-      <div class="flex justify-between items-center">
-        <p class="font-bold font-lg">{{ title }}</p>
-        <button class="btn btn-sm btn-circle" onclick="popup.close()">
-          <Icon name="fe:close" />
-        </button>
-      </div>
-      <slot />
+      <slot name="content">
+        <div class="flex justify-between items-center">
+          <p class="font-bold font-lg">{{ title }}</p>
+          <button class="btn btn-sm btn-circle" onclick="popup.close()">
+            <Icon name="fe:close" />
+          </button>
+        </div>
+        <slot />
+      </slot>
     </div>
   </dialog>
 </template>
--- components/settings/SettingsView.vue
+++ components/settings/SettingsView.vue
@@ -1,14 +1,14 @@
 <script setup lang="ts">
 const category = [
   {
-    name: 'Appearances',
-    link: '/settings/appearances',
-    icon: 'pepicons-pencil:paint-pallet'
+    name: "Appearances",
+    link: "/settings/appearances",
+    icon: "pepicons-pencil:paint-pallet",
   },
   {
-    name: 'Preferences',
-    link: '#',
-    icon: 'hugeicons:preference-vertical',
+    name: "Preferences",
+    link: "#",
+    icon: "hugeicons:preference-vertical",
     disabled: true,
   },
 ];
@@ -17,32 +17,51 @@ const user = useUserStore();
 
 const handleLogout = () => {
   user.logoutUser();
-  document.querySelector('#popup')?.close();
+  document.querySelector("#popup")?.close();
 };
 </script>
 
 <template>
-  <Modal title="Confirm">
-    <div class="mt-4">
-      <p class="text-lg my-2">Are you sure you want to Logout?</p>
-      <div class="flex justify-end gap-x-2">
-        <button class="btn bg-red-500 rounded-md" @click="handleLogout">Yes</button>
-        <button class="btn border rounded-md" onclick="popup.close()">No</button>
+  <Modal>
+    <template #content>
+      <div class="p-3">
+        <div class="flex flex-col items-center my-3 gap-y-3">
+          <Icon
+            name="teenyicons:logout-solid"
+            class="text-red-500"
+            :size="32"
+          />
+          <p class="text-xl font-bold">Confirm Logout</p>
+          <p class="text-sm">Are you sure you want to Logout?</p>
+        </div>
+        <div class="flex flex-col justify-end gap-y-4">
+          <button class="btn bg-red-500 rounded-md" @click="handleLogout">
+            Yes
+          </button>
+          <button class="btn border rounded-md" onclick="popup.close()">
+            No
+          </button>
+        </div>
       </div>
-    </div>
+    </template>
   </Modal>
   <ul>
     <template v-for="item in category" :key="item.name">
       <NuxtLink :to="item.link">
-        <li class="bg-base-200 my-4 p-3 rounded-md flex items-center gap-x-2"
-          :class="{ 'cursor-not-allowed opacity-50': item.disabled }">
+        <li
+          class="bg-base-200 my-4 p-3 rounded-md flex items-center gap-x-2"
+          :class="{ 'cursor-not-allowed opacity-50': item.disabled }"
+        >
           <Icon v-if="item.icon" :name="item.icon" />
           {{ item.name }}
         </li>
       </NuxtLink>
     </template>
-    <li v-if="user.jwt" class="bg-red-500 my-4 p-2 rounded-md flex items-center gap-x-2 justify-center text-gray-300"
-      onclick="popup.showModal()">
+    <li
+      v-if="user.jwt"
+      class="bg-red-500 my-4 p-2 rounded-md flex items-center gap-x-2 justify-center text-gray-300"
+      onclick="popup.showModal()"
+    >
       <Icon name="hugeicons:logout-04" />
       Logout
     </li>
