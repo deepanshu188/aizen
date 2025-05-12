c9c03a21af14da9d8327f9dc2c23aa3b67cde1f4
deepanshu188 <deepanshubisht188@gmail.com>
2025-05-12 20:36:12 +0530

    updated: handled the markdown overflowing alltogether

--- components/comments/CommentCard.vue
+++ components/comments/CommentCard.vue
@@ -19,15 +19,26 @@ const savePost = async (payload: Object) => {
       <div class="flex justify-between items-center">
         <NuxtLink :to="`/user/${c?.creator?.id}`">
           <div class="inline-flex items-center gap-x-2" role="button">
-            <Avatar :name="capitalFirst(c?.creator?.name)" :image="c?.creator?.avatar" :alt="c?.creator?.name" />
+            <Avatar
+              :name="capitalFirst(c?.creator?.name)"
+              :image="c?.creator?.avatar"
+              :alt="c?.creator?.name"
+            />
             <p class="text-sm">{{ c.creator.name }}</p>
           </div>
         </NuxtLink>
         <p class="text-sm">{{ diffDays(c.counts?.published) }}</p>
       </div>
-      <p class="my-2 md-style overflow-x-auto" v-html="renderMd(c?.comment?.content)"></p>
-      <Interactions :my_vote="c?.my_vote" :post="c.post" :counts="c.counts" :saved="c.saved" @vote="handleVote"
-        @save="savePost" minimal />
+      <p class="my-2 md-style" v-html="renderMd(c?.comment?.content)"></p>
+      <Interactions
+        :my_vote="c?.my_vote"
+        :post="c.post"
+        :counts="c.counts"
+        :saved="c.saved"
+        @vote="handleVote"
+        @save="savePost"
+        minimal
+      />
     </div>
     <div class="mt-2" v-if="c.comment.children.length > 0">
       <NestedComments :data="c.comment.children" />
