eb0b6063c75a811424dd835bfffe960c2837a740
deepanshu188 <deepanshubisht188@gmail.com>
2025-05-14 22:13:27 +0530

    added: custom select component

---
+++ components/UI/Select.vue
@@ -0,0 +1,22 @@
+<script setup lang="ts">
+const model = defineModel();
+interface Props {
+  label?: string;
+  options?: { name: string; value: string }[];
+}
+
+defineProps<Props>();
+</script>
+
+<template>
+  <fieldset class="fieldset">
+    <legend class="fieldset-legend">{{ label }}</legend>
+    <select class="select select-bordered w-full md:max-w-xs" v-model="model">
+      <slot>
+        <option v-for="option in options" :key="option.value" :value="option.value">
+          {{ option.name }}
+        </option>
+      </slot>
+    </select>
+  </fieldset>
+</template>
